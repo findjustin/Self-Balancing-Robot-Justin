21a356f889f67a18159f7359995ab72f46df5c05
JustinC <justinchen@justins-imac-2.att.net>
2014-08-04 18:06:52 -0700

    code

---
+++ Librariessss/DualVNH5019MotorShield/sketch_jun20a/sketch_jun20a.ino
@@ -0,0 +1,22 @@
+#include "DualVNH5019MotorShield.h"
+
+DualVNH5019MotorShield md;
+
+
+int speed;
+
+void setup()
+{
+Serial.begin(115200);
+Serial.println("hello people");
+md.init();
+}
+
+void loop()
+{
+md.setM1Speed(400);
+delay(5000);
+md.setM1Brake(100);
+//delay(5000);
+
+}
